b4085f80d2bfe944ebfd6e2b4d4f1349fc21b476
testpkbot <58456243+testpkbot@users.noreply.github.com>
2019-12-03 14:13:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,15 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command(name="ミミッキュ")
+async def mimikkyu(ctx):
+    await ctx.send(f"H:55 A:90 B:80 C:50 D:105 S:96")
+
+
+@bot.command(name="さようなら")
+async def goodbye(ctx):
+    await ctx.send(f"じゃあね、{ctx.message.author.name}さん！")
 
 token = os.environ['DISCORD_BOT_TOKEN']
 bot.run(token)
